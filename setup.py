637baae5d80b3b7fb3f69a49a59a7f14a1bf07d5
Anonymous User <65958272+vtamburino@users.noreply.github.com>
2020-07-10 12:52:05 -0400

    Add New GenericMotionFamilyLaw

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
 setup(name='docassemble.GenericMotionFamilyLaw',
       version='0.0.1',
       description=('A docassemble extension.'),
-      long_description='# docassemble.GenericMotionFamilyLaw\n\nA docassemble extension.\n\n## Author\n\nvtamburino@su.suffolk.edu\n\n',
+      long_description='# docassemble.GenericMotionFamilyLaw\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nvtamburino@su.suffolk.edu\r\n\r\n',
       long_description_content_type='text/markdown',
       author='',
       author_email='vtamburino@su.suffolk.edu',
