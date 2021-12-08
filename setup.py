597ad32d46f5edb2571199012584c48801517a91
Quinten Steenhuis <qsteenhuis@gmail.com>
2021-12-08 15:08:59 -0500

    Fix dependencies

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(name='docassemble.GenericMotionFamilyLaw',
       url='https://docassemble.org',
       packages=find_packages(),
       namespace_packages=['docassemble'],
-      install_requires=['docassemble.MACourts>=0.0.58.2'],
+      install_requires=['docassemble.AssemblyLine>=2.7.1'],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/GenericMotionFamilyLaw/', package='docassemble.GenericMotionFamilyLaw'),
      )
